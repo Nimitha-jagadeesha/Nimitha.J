52af4408f2dfc41c1651444f0e8776f789958d07
Nimitha J <nimitha1jagadeesha@gmail.com>
2023-02-10 21:40:37 +0530

    Coding Profiles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Projects from './Projects';
 import Introduction from './Introduction';
+import CodingProfiles from './CodingProfiles';
 function App() {
   return (
     <>
       <Introduction />
       <Projects />
+      <CodingProfiles/>
     </>
   );
 }
---
+++ src/CodingProfiles.js
@@ -0,0 +1,26 @@
+export default function CodingProfiles() {
+    return (
+        <div className="Coding">
+            <br />
+            <h3 className="text-center">Coding Profiles</h3>
+            <br />
+            <br />
+            <div className="row">
+                <a href="https://www.hackerrank.com/nimithajagadees" className="col-1 offset-1">
+                    <img src={process.env.PUBLIC_URL + "hackerrank.png"} height={50} width={50} />
+                </a>
+                <a href="https://auth.geeksforgeeks.org/user/nimitha1jagadeesha/" className="col-1 offset-1">
+                    <img src={process.env.PUBLIC_URL + "geeks.png"}  height={50} width={50}/>
+                </a>
+                <a href="https://leetcode.com/Nimitha_J/" className="col-1 offset-1">
+                    <img src={process.env.PUBLIC_URL + "leetcode.webp"} height={50} width={50} />
+                </a>
+                <a href="https://www.codechef.com/users/nimitha1j" className="col-1 offset-1">
+                    <img src={process.env.PUBLIC_URL + "codechef.png"}  height={50} width={50} />
+                </a>
+            </div>
+            <br />
+            <br />
+        </div>
+    )
+}
