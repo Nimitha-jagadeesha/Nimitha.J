14bd2912692d638007e1f9cba932b671621f504a
Nimitha J <nimitha1jagadeesha@gmail.com>
2023-02-10 21:06:16 +0530

    Added Projects

---
+++ src/Introduction.js
@@ -0,0 +1,22 @@
+export default function Introduction() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Software Developer</h1>
+            </header>
+            <br />
+            <div className="row">
+                <div className='col-md-3 col-9 offset-2 offset-md-1'>
+                    <img src={process.env.PUBLIC_URL + "favicon.png"} height={300} alt="Dream->code->Achieve" />
+                </div>
+                <ul className="col-md-6 col-10 offset-1">
+                    <li>👋 Hi, I’m Nimitha J, a passinate Software developer.</li>
+                    <li>👀 I’m interested in solving problems, learning different technologies and teckstacks.</li>
+                    <li>🌱 I’m currently learning best coding practices and System design :)</li>
+                    <li>✨ I am continually inspired by the limitless potential of software development to bring ideas to life.</li>
+                    <li>☕ I enjoy coding while sipping on a cup of tea.</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
---
+++ src/Projects.js
@@ -0,0 +1,29 @@
+export default function Projects() {
+    return (
+        <div className="Projects">
+            <h3 className="text-center">👨‍💻 My Projects</h3>
+            <ul>
+                <li>
+                    Full Stack Web App
+                    <ul>
+                        <li ><a href="https://github.com/Nimitha-jagadeesha/Epsilon-21.0" className="App-link">Epsilon</a> (Django, Postgress SQL, HTML, CSS, Bootstrap, JS)</li>
+                        <li><a href="https://github.com/Nimitha-jagadeesha/Impetus" className="App-link">Impetus 21.0 </a> (MongoDB, nodejs, HTML, CSS, JavaScript)</li>
+                        <li>Agriculturify:
+                            <ul>
+                                <li><a href="https://github.com/Nimitha-jagadeesha/Agricutureify" className="App-link">Frontend </a>(React, HTML, CSS, Javascript)</li>
+                                <li><a href="https://github.com/Nimitha-jagadeesha/AGRO-Backend-API" className="App-link"> Backend</a> (nodejs, express, MongDB)</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li> Mobile Application Development
+                    <ul>
+                        <li>
+                            <a href="https://github.com/Nimitha-jagadeesha/Covid-care" className="App-link"> Covid Care </a>(Java, XML, Firebase)
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    )
+}
