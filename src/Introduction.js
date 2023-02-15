11a5e781b65962fa07746bf36e50df21c42321db
Nimitha J <nimitha1jagadeesha@gmail.com>
2023-02-15 19:58:38 +0530

    Styles in Intro

--- src/Introduction.js
+++ src/Introduction.js
@@ -1,16 +1,14 @@
 export default function Introduction() {
     return (
         <div className="App" id="Intro">
-            <header className="App-header">
-                <h1>Software Developer</h1>
-            </header>
-            <br />
             <div className="row">
                 <div className='col-md-3 col-9 offset-2 offset-md-1' data-aos="zoom-in" data-aos-duration="2000">
                     <img src={process.env.PUBLIC_URL + "favicon.png"} height={300} alt="Dream->code->Achieve" />
                 </div>
                 <ul className="col-md-6 col-10 offset-1" data-aos="fade-up" data-aos-duration="2000">
-                    <li>👋 Hi, I’m Nimitha J, a passinate Software developer.</li>
+                    <h1 className="text-center heading">Nimitha J  <span class="wave">🖐🏻</span> </h1>
+                    <h5 className="tagline"> - Passinate Software Developer</h5>
+                    <br/>
                     <li>👀 I’m interested in solving problems, learning different technologies and teckstacks.</li>
                     <li>🌱 I’m currently learning best coding practices and System design :)</li>
                     <li>✨ I am continually inspired by the limitless potential of software development to bring ideas to life.</li>
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,6 @@
 export default function Navigation() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
         <img src={process.env.PUBLIC_URL + "favicon.png"} height={30} alt="Dream->code->Achieve" />
             <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
