bc0f77e7c007a90bf4eb3dd20ea37934e0fce7a5
Nimitha J <58389915+Nimitha-jagadeesha@users.noreply.github.com>
2024-10-20 19:00:51 +0530

    Create certifications.js

---
+++ src/certifications.js
@@ -0,0 +1,14 @@
+export default function Certificates() {
+    return (
+        <div className="Projects" id="Projects" data-aos="fade-up" data-aos-duration="2000">
+            <h3 className="text-center">Certifications</h3>
+          <ul>
+              <li> <a href="https://www.credly.com/badges/f04a3e6f-b447-4d1f-9ebd-cc3557c22667">AWS Certified Cloud Practitioner</a> Issued by Amazon Web Services Training and Certification on September 22, 2023</li>
+              <li> <a href="https://www.coursera.org/account/accomplishments/verify/DBQ92D39TUWF">Front-End Web Development with React</a> Issued by The Hong Kong University of Science and Technology on July 3, 2020</li>
+              <li> <a href="https://www.coursera.org/account/accomplishments/verify/DC6VCDSD6C5R">Multiplatform Mobile App Development with React Native</a>  Issued by The Hong Kong University of Science and Technology on July 9, 2020</li>
+              <li> <a href="https://www.udemy.com/certificate/UC-ede799b1-df0a-4f47-91d9-6278b98b78a5">Android Application Development</a>issued by  ProgramMe Programming on May 2, 2020</li> 
+          </ul>
+          
+        </div>
+    )
+}
